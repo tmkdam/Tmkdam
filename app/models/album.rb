2bdc5cc68d2be01f2ecb945201461deae3b281c0
John <john@Tahas-MacBook-Pro-Ret.local>
2016-06-16 14:10:26 -0500

    Task #4: Added new scope recent, which take a an argument n, and returns n number or ablums from the past 4 months

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,4 +5,5 @@ class Album < ActiveRecord::Base
   validates :name, presence: true, length: { in: 2..50 }
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
+  scope :recent,  -> (n) { where("released_on >= '#{(Date.today - 4.months)}'").limit(n) }
 end
