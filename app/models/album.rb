96b1a08f9195eac1a1d961e01d428c1429773053
John <john@Tahas-MacBook-Pro-Ret.local>
2016-06-16 15:18:20 -0500

    Task #6: added validation for publisher to be required before proceeding

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,6 +4,7 @@ class Album < ActiveRecord::Base
   
   validates :name, presence: true, length: { in: 2..50 }
   validates :cover_art, presence: true
+  validates :publisher_id, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
   scope :recent,  -> (n) { where("released_on >= '#{(Date.today - 4.months)}'").limit(n) }
 end
