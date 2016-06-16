bf56f74e1d58ce427e2139a061df6842d703ba83
John <john@Tahas-MacBook-Pro-Ret.local>
2016-06-16 17:11:53 -0500

    Task #7: Added latest albums to footer and added caching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
 	def get_newest_album
 		Album.order(:released_on).last.id
 	end
+
+	def get_four_lateset_albums
+		Rails.cache.fetch("#{self}/get_four_lateset_albums", expires_in: 1.hours) do
+			Album.recent(4)
+	    end
+	end
 end
